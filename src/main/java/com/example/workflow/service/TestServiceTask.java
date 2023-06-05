d24105a903a2490437c37617c28eb785a325c74c
Ali <mohajer13sam@gmail.com>
2023-06-05 05:35:47 +0330

    get-my-incidents-{processInstanceId}

--- src/main/java/com/example/workflow/service/TestServiceTask.java
+++ src/main/java/com/example/workflow/service/TestServiceTask.java
@@ -4,10 +4,8 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.engine.delegate.TaskListener;
 import org.camunda.bpm.engine.impl.incident.IncidentContext;
-import org.camunda.bpm.engine.impl.incident.IncidentHandling;
-import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.impl.persistence.entity.IncidentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestServiceTask implements JavaDelegate {
@@ -18,38 +16,26 @@ public class TestServiceTask implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) {
 
-//        Execution delegateExecution = runtimeService.createExecutionQuery().processInstanceId("4d047960-021d-11ee-8a94-7c8bca06c3f6").singleResult();
-
-
-//        execution.
+//        IncidentContext context= new IncidentContext();
+//        context.setProcessDefinitionId(execution.getProcessDefinitionId());
+//        context.setActivityId(execution.getCurrentActivityId());
+//        context.setExecutionId(execution.getId());
+//        context.setConfiguration("configuration");
+//        context.setTenantId(execution.getTenantId());
+//        context.setJobDefinitionId(execution.getProcessDefinitionId());
+//        context.setHistoryConfiguration("historyConfiguration");
+//        context.setFailedActivityId(execution.getCurrentActivityId());
 //
-//        try {
-//            throw new RuntimeException();
-//        } catch (Exception ex) {
-//            execution.createIncident("this is incident Type", "this isConfig", "this is message");
-//        }
+        IncidentEntity newIncident = IncidentEntity.createAndInsertIncident("failedJob", context, "failedJob");
+        newIncident.createRecursiveIncidents();
+        newIncident.
 
-//        protected String processDefinitionId;
-//        protected String activityId;
-//        protected String executionId;
-//        protected String configuration;
-//        protected String tenantId;
-//        protected String jobDefinitionId;
-//        protected String historyConfiguration;
-//        protected String failedActivityId;
-
-        IncidentContext context= new IncidentContext();
-        context.setProcessDefinitionId(execution.getProcessDefinitionId());
-        context.setActivityId(execution.getCurrentActivityId());
-        context.setExecutionId(execution.getId());
-        context.setConfiguration("configuration");
-        context.setTenantId(execution.getTenantId());
-        context.setJobDefinitionId(execution.getProcessDefinitionId());
-        context.setHistoryConfiguration("historyConfiguration");
-        context.setFailedActivityId(execution.getCurrentActivityId());
-
-
-        IncidentHandling.createIncident("this is incident Type", context, "this is message");
+        try {
+            throw new RuntimeException();
+        } catch (Exception ex) {
+            execution.createIncident("rUUnTIME","ConFiGuraTion","MesSaGe");
+            execution.resolveIncident("");
+        }
 
     }
 
@@ -59,16 +45,7 @@ public class TestServiceTask implements JavaDelegate {
 //
 //
 //        delegateTask.getExecution().createIncident("this is incident Type2", "this isConfig2", "this is message2");
+//        delegateTask.getExecution().resolveIncident("");
 //    }
-
-
-
-    private void createIncident(DelegateExecution execution){
-        execution.createIncident("this is incident Type", "this isConfig", "this is message");
-
-
-
-    }
-
 
 }
